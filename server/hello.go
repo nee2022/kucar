2e4ae3eff37fe3a420ab3a627ae29a3229f6bab2
zhangyihua <zhangyihua_2020@qq.com>
2021-11-02 23:00:58 +0800

    use trip.pb.go

--- server/hello.go
+++ server/hello.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	trippb "coolcar/proto/gen/go"
@@ -8,12 +9,31 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-func main(){
+func main() {
 	trip := trippb.Trip{
-		Start: "abc",
-		End: "def",
+		Start:       "abc",
+		End:         "def",
 		DurationSec: 3600,
-		FeeCent: 10000,
+		FeeCent:     10000,
+		StartPos: &trippb.Location{
+			Latitude:  30,
+			Longitude: 120,
+		},
+		EndPos: &trippb.Location{
+			Latitude:  35,
+			Longitude: 115,
+		},
+		PathLocations: []*trippb.Location{
+			{
+				Latitude:  31,
+				Longitude: 119,
+			},
+			{
+				Latitude:  32,
+				Longitude: 118,
+			},
+		},
+		Status: trippb.TripStatus_IN_PROGRESS,
 	}
 	fmt.Println(&trip)
 	b, err := proto.Marshal(&trip)
@@ -23,9 +43,15 @@ func main(){
 	fmt.Printf("%X\n", b)
 
 	var trip2 trippb.Trip
-	err = proto.Unmarshal(b,&trip2)
+	err = proto.Unmarshal(b, &trip2)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(&trip2)
+
+	b, err = json.Marshal(&trip2)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", b)
 }
