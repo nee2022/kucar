18ec78be42fe4688f6e89802b123fcd46901786a
zhangyihua <zhangyihua_2020@qq.com>
2021-11-02 16:02:43 +0800

    hello.go add marshal()

--- server/hello.go
+++ server/hello.go
@@ -12,5 +12,10 @@ func main(){
 		DurationSec: 3600,
 		FeeCent: 10000,
 	}
-	fmt.Println(&trip)
+	fmt.Println(trip)
+	b, err := proto.Marshal(&trip)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%X\n", b)
 }
